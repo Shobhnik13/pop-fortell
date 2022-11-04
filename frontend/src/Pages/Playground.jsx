a7e821a3c3bd9a1e46a0494022dd6dba70121bd6
Vishal Chauhan <44545745+vish-han@users.noreply.github.com>
2022-11-04 11:57:42 +0530

    Images and Gifs Added

---
+++ frontend/src/Pages/Playground.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Playground = () => {
+  return (
+    <div className="bg-black h-[100vh] bg-[url('./assets/tiles.svg')]">
+        
+    </div>
+  )
+}
+
+export default Playground
