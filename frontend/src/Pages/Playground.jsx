68b897e2b22254083cab6fed03f9a9f37336ec33
Vishal Chauhan <44545745+vish-han@users.noreply.github.com>
2022-11-05 11:17:04 +0530

    Playground Started

--- frontend/src/Pages/Playground.jsx
+++ frontend/src/Pages/Playground.jsx
@@ -5,7 +5,8 @@ const Playground = () => {
   return (
     <div className="bg-black h-[100vh] bg-[url('./assets/tiles.svg')]">
       <Navbar/>
-        <h1 className="text-white text-2xl" >Playground</h1>
+        <h1 className="text-fuchsia-500 text-center text-5xl anton mt-3" >Predict and Let the Fun Begin</h1>
+        
     </div>
   )
 }
