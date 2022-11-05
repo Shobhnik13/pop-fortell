51c4e3fb3b3667cd27f05b0b440dbf35f3aaae4c
shobhnik13 <shobhnikw@gmail.com>
2022-11-05 22:51:40 +0530

    ques file added

---
+++ frontend/src/questions/crick.js
@@ -0,0 +1,50 @@
+export const cricket=[
+    {
+        id:"1",
+        question:"which team will win the  T20i world cup 2022?",
+        option1:"india",
+        option2:"New zealand",
+        option3:"England",
+        option4:"South Africa",
+        answer:"",
+
+
+},
+{
+    id:"2",
+    question:"who will be the man of the series of t20i world cup 2022?",
+    option1:"Virat kohli",
+    option2:"Rilee rossouw",
+    option3:"Sikandar raza",
+    option4:"None of the above",
+    answer:"",
+
+},
+{
+    id:"3",
+    question:"which team will win the women's icc cricket odi world cup 2025?",
+    option1:"india",
+    option2:"australia",
+    option3:"England",
+    option4:"None of the above",
+
+},
+{
+    id:"4",
+    question1:"which team will win the icc men's odi world cup 2023?",
+    option1:"Australia",
+    option2:"England",
+    option3:"Pakistan",
+    option4:"None of the above",
+    answer:"",
+},
+{
+    id:"5",
+    question1:"Which team will win the Test World cup Championship 2023?",
+    option1:"New zealand",
+    option2:"England",
+    option3:"India",
+    option4:"None of the above",
+
+}
+]
---
+++ frontend/src/questions/ques.js
@@ -0,0 +1,43 @@
+export const question=[
+    {
+        id:"1",
+        question:"Will X-men be introduced in Wakanda forever?",
+        option1:"yes",
+        option2:"no",
+        answer:""
+    },
+    {
+        id:"2",
+        question:"What happens to jake in avatar 2?",
+        option1:"Taken back to earth",
+        options2:"Gets tricked by RDA ",
+        option3:"Jake dies",
+        answer:"",
+
+    },
+    {
+        id:"3",
+        question:"what role will Kang the conqueror play in Ant-Man and the Wasp: Quantumania?",
+        option1:"Hero",
+        option2:"Anti-hero",
+        option3:"Villian",
+        answer:"",
+    
+    },
+    {
+        id:"4",
+        question:"what realtion will be shown between `Kang The Conqueror` and Shang-Chi's Ten Rings",
+        option1:"Kang made the rings",
+        option2:"rings are the only thing to stand against Kang The Conqueror",
+        option3:"no relation",
+        option4:"Kang uses those rings to travel in multiverse",
+        answer:"",
+
+    },
+    {   id:"5",
+        question:"Will there be a fight sequence between Charon and John wick in John Wick: Chapter 4",
+        option1:"yes",
+        option2:"no",
+        answer:"",
+    }
+]
