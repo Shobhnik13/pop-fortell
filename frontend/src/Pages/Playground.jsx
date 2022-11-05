e3089110ad6a065653926bcf5cbd4d408d45d8e7
shobhnik13 <shobhnikw@gmail.com>
2022-11-05 14:49:30 +0530

    vote component added

--- frontend/src/Pages/Playground.jsx
+++ frontend/src/Pages/Playground.jsx
@@ -4,9 +4,10 @@ import VoteComp from '../Components/VoteComp'
 
 const Playground = () => {
   return (
-    <div className="bg-black h-[100vh] bg-[url('./assets/tiles.svg')]">
+    <div className="bg-black h-[160vh] bg-[url('./assets/tiles.svg')]">
       <Navbar/>
         <h1 className="text-white text-2xl" >Playground</h1>
+    <VoteComp/>
     </div>
   )
 }
