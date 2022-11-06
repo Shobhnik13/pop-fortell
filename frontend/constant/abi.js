e948c7e3c1017bfc073ba1e842d750961ff9b6aa
Vishal Chauhan <44545745+vish-han@users.noreply.github.com>
2022-11-06 09:32:01 +0530

    Question Mapping Done

---
+++ frontend/constant/abi.js
@@ -0,0 +1,77 @@
+export const address='0x3F467d48D38ed28a9Fe3239409d9d16b018d26E9';
+export const abi=[
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "name": "QuesAndAns",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "_id",
+                "type": "uint256"
+            },
+            {
+                "internalType": "uint256",
+                "name": "_andID",
+                "type": "uint256"
+            }
+        ],
+        "name": "checkAnswer",
+        "outputs": [
+            {
+                "internalType": "bool",
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "stateMutability": "payable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "i",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "_id",
+                "type": "uint256"
+            },
+            {
+                "internalType": "uint256",
+                "name": "_andID",
+                "type": "uint256"
+            }
+        ],
+        "name": "setAnswer",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+]
--- frontend/src/Components/VoteComp.jsx
+++ frontend/src/Components/VoteComp.jsx
@@ -1,17 +1,28 @@
 import React from 'react'
-
+import {Contract, ethers} from 'ethers'
+import { address,abi } from '../../constant/abi'
 const VoteComp = () => {
+  async function checkOption(_id){
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const signer = provider.getSigner()
+     const checkContract=new Contract(address,abi,signer);
+    const result =await checkContract.checkAnswer(4,_id);
+
+    console.log(result);
+     
+    
+   }  
   return (
     <div className=' mx-4  justify-evenly mt-6 font2 font-medium'>
 
-        <div className='main w-[45%] bg-[#101010] p-[30px] hover:cursor-pointer rounded-2xl hover:bg-black '>
+        <div className='main w-[45%] bg-[#101010] p-[30px] hover:cursor-pointer rounded-2xl hover:bg-black ' >
             <div className='text-white question'><span className='  font-semiboldbold text-2xl text-fuchsia-500'>Fan theory: </span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde laudantium atque illum, nisi dolore enim accusamus odit fuga! Porro, illum.</div>
         <div className='options text-white'><span className='font-semiboldbold text-2xl text-fuchsia-500'>Options:</span></div>
      <div className="optionsbox flex flex-row flex-wrap ">
-        <div className='optionbtn text-black; w-[40%] rounded-xl m-3 p-2 hover:cursor-pointer text-lg hover:bg-[#101010]'><span className='font-bold'>A: </span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, est!</div>
-        <div className='optionbtn text-black w-[40%] rounded-xl m-3  p-2 hover:cursor-pointer hover:bg-[#101010]'><span className='font-bold'>B: </span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, est!</div>
-        <div className='optionbtn text-black w-[40%] rounded-xl m-3  p-2 hover:cursor-pointer hover:bg-[#101010]'><span className='font-bold'>C: </span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, est!</div>
-        <div className='optionbtn text-black w-[40%] rounded-xl m-3  p-2 hover:cursor-pointer hover:bg-[#101010]'><span className='font-bold'>D: </span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, est!</div>
+        <div className='optionbtn text-black; w-[40%] rounded-xl m-3 p-2 hover:cursor-pointer text-lg hover:bg-[#101010]' onClick={checkOption(1)}><span >A: </span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, est!</div>
+        <div className='optionbtn text-black w-[40%] rounded-xl m-3  p-2 hover:cursor-pointer hover:bg-[#101010]'><span className='font-bold' onClick={checkOption(2)}>B: </span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, est!</div>
+        <div className='optionbtn text-black w-[40%] rounded-xl m-3  p-2 hover:cursor-pointer hover:bg-[#101010]'><span className='font-bold' onClick={checkOption(3)}>C: </span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, est!</div>
+        <div className='optionbtn text-black w-[40%] rounded-xl m-3  p-2 hover:cursor-pointer hover:bg-[#101010]'><span className='font-bold' onClick={checkOption(4)}>D: </span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, est!</div>
      </div>
         </div>
         {/* ------------------------------------ */}
